580626e6e324c6836145852485866dcbff9a3740
Newdigate <nicnewdigate@gmail.com>
2020-10-03 14:11:26 +0100

    working, with some distortion

--- output_spi.cpp
+++ output_spi.cpp
@@ -34,13 +34,36 @@
 #include <SPI.h>
 #include <imxrt.h>
 #include <cstdint>
+
+/* AD5754R Register Map */
+#define AD5754R_REG_DAC             0x00 // DAC register
+#define AD5754R_REG_RANGE_SELECT    0x01 // Output range select register
+#define AD5754R_REG_POWER_CONTROL   0x02 // Power control register
+#define AD5754R_REG_CONTROL         0x03 // Control register
+
+/* AD5754R Channel Address */
+#define AD5754R_DAC_A               0x00 // Address of channel A
+#define AD5754R_DAC_B               0x01 // Address of channel B
+#define AD5754R_DAC_C               0x02 // Address of channel C
+#define AD5754R_DAC_D               0x03 // Address of channel D
+#define AD5754R_DAC_ALL             0x04 // All four DACs
+
+
+/* AD5754R Range Bits */
+#define AD5754R_UNIPOLAR_5_RANGE    0x00 // 0..+5(V)
+#define AD5754R_UNIPOLAR_10_RANGE   0x01 // 0..+10(V)
+#define AD5754R_UNIPOLAR_10_8_RANGE 0x02 // 0..+10.8(V)
+#define AD5754R_BIPOLAR_5_RANGE     0x03 // -5..+5(V)
+#define AD5754R_BIPOLAR_10_RANGE    0x04 // -10...+10(V)
+#define AD5754R_BIPOLAR_10_8_RANGE  0x05 // -10.8...+10.8(V)
+
 audio_block_t * AudioOutputSPI::block_input[8] = {
 	NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL
 };
 bool AudioOutputSPI::update_responsibility = false;
 
 unsigned int AudioOutputSPI::read_index = 0;
-unsigned int AudioOutputSPI::DA_SYNC = 0;
+unsigned int AudioOutputSPI::DA_SYNC = 16;
 volatile uint8_t AudioOutputSPI::buf[6] = {0,0,0,0,0,0};
 int AudioOutputSPI::voltages[8] = {0,0,0,0,0,0,0,0};
 unsigned int AudioOutputSPI::bytesReceived = 0;
@@ -50,10 +73,61 @@ DMAChannel AudioOutputSPI::dma(false);
 void AudioOutputSPI::begin(void)
 {
     pinMode(DA_SYNC, OUTPUT);
-    
-	SPI.begin();
+    digitalWrite(DA_SYNC, HIGH);
+	SPI1.begin();
+    delay(1);
 
-	Serial.print("void AudioOutputSPI::begin(void)\n");
+    SPI1.beginTransaction(SPISettings());
+    digitalWrite(DA_SYNC, LOW);
+    uint8_t configureDac[] = {
+            0x10,
+            0x00,
+            0x0f,
+            0x10,
+            0x00,
+            0x0f
+    };
+    SPI1.transfer(configureDac, 6);
+    SPI1.endTransaction();
+    digitalWrite(DA_SYNC, HIGH);
+
+    delay(1);
+
+    digitalWrite(DA_SYNC, LOW);
+    uint8_t configureDacVoltageRange[] = {
+            0x28,
+            0x00,
+            AD5754R_UNIPOLAR_10_RANGE,
+            0x28,
+            0x00,
+            AD5754R_UNIPOLAR_10_RANGE
+    };
+
+    SPI1.beginTransaction(SPISettings());
+    SPI1.transfer(configureDacVoltageRange, 6);
+    SPI1.endTransaction();
+
+    digitalWrite(DA_SYNC, HIGH);
+
+    delay(1);
+
+    digitalWrite(DA_SYNC, LOW);
+    uint8_t configureDacVoltageRange2[] = {
+            0x0C,
+            0x00,
+            0x00,
+            0x0C,
+            0x00,
+            0x00
+    };
+
+    SPI1.beginTransaction(SPISettings());
+    SPI1.transfer(configureDacVoltageRange2, 6);
+    SPI1.endTransaction();
+
+    digitalWrite(DA_SYNC, HIGH);
+
+    delay(1);
 
     config_spi();
     config_dma();
@@ -67,15 +141,15 @@ void AudioOutputSPI::begin(void)
 void AudioOutputSPI::isr(void)
 {
     dma.clearInterrupt();
-    SPI.endTransaction();
+    SPI1.endTransaction();
 
-    IMXRT_LPSPI4_S.FCR = LPSPI_FCR_TXWATER(15); // _spi_fcr_save; // restore the FSR status...
-    IMXRT_LPSPI4_S.DER = 0;
-    IMXRT_LPSPI4_S.CR = LPSPI_CR_MEN | LPSPI_CR_RRF | LPSPI_CR_RTF; // actually clear both...
-    IMXRT_LPSPI4_S.SR = 0x3f00;    // clear out all of the other status...
+    IMXRT_LPSPI3_S.FCR = LPSPI_FCR_TXWATER(15); // _spi_fcr_save; // restore the FSR status...
+    IMXRT_LPSPI3_S.DER = 0;
+    IMXRT_LPSPI3_S.CR = LPSPI_CR_MEN | LPSPI_CR_RRF | LPSPI_CR_RTF; // actually clear both...
+    IMXRT_LPSPI3_S.SR = 0x3f00;    // clear out all of the other status...
 
-    while (IMXRT_LPSPI4_S.FSR & 0x1f);
-    while (IMXRT_LPSPI4_S.SR & LPSPI_SR_MBF) ;  //Status Register? Module Busy flag
+    while (IMXRT_LPSPI3_S.FSR & 0x1f);
+    while (IMXRT_LPSPI3_S.SR & LPSPI_SR_MBF) ;  //Status Register? Module Busy flag
     digitalWrite(DA_SYNC, HIGH);
 
     bytesReceived++;
@@ -93,7 +167,7 @@ void AudioOutputSPI::isr(void)
 
     beginTransfer();
 }
-
+const uint32_t zero_level = 0xFFFF / 2;
 void AudioOutputSPI::beginTransfer()
 {
     if (bytesReceived == 0) {
@@ -101,14 +175,14 @@ void AudioOutputSPI::beginTransfer()
         read_index++;
         if (read_index == 128) return;
 
-        voltages[0] = (block_input[0] != NULL) ? block_input[0]->data[read_index] : 0;
-        voltages[1] = (block_input[1] != NULL) ? block_input[1]->data[read_index] : 0;
-        voltages[2] = (block_input[2] != NULL) ? block_input[2]->data[read_index] : 0;
-        voltages[3] = (block_input[3] != NULL) ? block_input[3]->data[read_index] : 0;
-        voltages[4] = (block_input[4] != NULL) ? block_input[4]->data[read_index] : 0;
-        voltages[5] = (block_input[5] != NULL) ? block_input[5]->data[read_index] : 0;
-        voltages[6] = (block_input[6] != NULL) ? block_input[6]->data[read_index] : 0;
-        voltages[7] = (block_input[7] != NULL) ? block_input[7]->data[read_index] : 0;
+        voltages[0] = (block_input[0] != NULL) ? block_input[0]->data[read_index] + zero_level : zero_level;
+        voltages[1] = (block_input[1] != NULL) ? block_input[1]->data[read_index] + zero_level : zero_level;
+        voltages[2] = (block_input[2] != NULL) ? block_input[2]->data[read_index] + zero_level : zero_level;
+        voltages[3] = (block_input[3] != NULL) ? block_input[3]->data[read_index] + zero_level : zero_level;
+        voltages[4] = (block_input[4] != NULL) ? block_input[4]->data[read_index] + zero_level : zero_level;
+        voltages[5] = (block_input[5] != NULL) ? block_input[5]->data[read_index] + zero_level : zero_level;
+        voltages[6] = (block_input[6] != NULL) ? block_input[6]->data[read_index] + zero_level : zero_level;
+        voltages[7] = (block_input[7] != NULL) ? block_input[7]->data[read_index] + zero_level : zero_level;
 
         // first 3 bytes -> DAC0
         buf[0] = 0x00;              // channel == 0
@@ -123,22 +197,22 @@ void AudioOutputSPI::beginTransfer()
 
     digitalWrite(DA_SYNC, LOW);
 
-    IMXRT_LPSPI4_S.TCR = (IMXRT_LPSPI4_S.TCR & ~(LPSPI_TCR_FRAMESZ(31))) | LPSPI_TCR_FRAMESZ(7);
-    IMXRT_LPSPI4_S.FCR = 0;
+    IMXRT_LPSPI3_S.TCR = (IMXRT_LPSPI3_S.TCR & ~(LPSPI_TCR_FRAMESZ(31))) | LPSPI_TCR_FRAMESZ(7);
+    IMXRT_LPSPI3_S.FCR = 0;
     // Lets try to output the first byte to make sure that we are in 8 bit mode...
-    IMXRT_LPSPI4_S.DER = LPSPI_DER_TDDE;//| LPSPI_DER_RDDE; //enable DMA on both TX and RX
-    IMXRT_LPSPI4_S.SR = 0x3f00; // clear out all of the other status...
-    SPI.beginTransaction(SPISettings(14000000, MSBFIRST, SPI_MODE0));
+    IMXRT_LPSPI3_S.DER = LPSPI_DER_TDDE;//| LPSPI_DER_RDDE; //enable DMA on both TX and RX
+    IMXRT_LPSPI3_S.SR = 0x3f00; // clear out all of the other status...
+    SPI1.beginTransaction(SPISettings(12000000, MSBFIRST, SPI_MODE0));
     dma.enable();
 }
 
 
 void AudioOutputSPI::update(void)
 {
-    //if (IMXRT_LPSPI4_S.SR & LPSPI_SR_MBF) return;  //already busy
-    //if (IMXRT_LPSPI4_S.FSR & 0x1f) return;
+    //if (IMXRT_LPSPI3_S.SR & LPSPI_SR_MBF) return;  //already busy
+    //if (IMXRT_LPSPI3_S.FSR & 0x1f) return;
 
-	audio_block_t *prev[16];
+	audio_block_t *prev[8];
 	unsigned int i;
 
 	__disable_irq();
@@ -168,8 +242,8 @@ void AudioOutputSPI::config_dma(void)
     dma.TCD->DLASTSGA = 0;
     dma.TCD->BITER_ELINKNO = sizeof(buf);
     dma.TCD->CSR = DMA_TCD_CSR_INTMAJOR;
-    dma.TCD->DADDR = (void *)((uint32_t)&(IMXRT_LPSPI4_S.TDR));
-    dma.triggerAtHardwareEvent(DMAMUX_SOURCE_LPSPI4_TX);
+    dma.TCD->DADDR = (void *)((uint32_t)&(IMXRT_LPSPI3_S.TDR));
+    dma.triggerAtHardwareEvent(DMAMUX_SOURCE_LPSPI3_TX);
     dma.disableOnCompletion();
     dma.attachInterrupt(isr);
     dma.interruptAtCompletion();
